0d4ecc5a969908a5e79973bccab462f509326174
javython999 <infinity2544@gmail.com>
2023-11-06 08:58:28 +0900

    검색 조건, 결과 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,16 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
-public class QuerydslBasicTest {
+class QuerydslBasicTest {
 
     @Autowired
     EntityManager em;
@@ -24,7 +27,7 @@ public class QuerydslBasicTest {
     JPAQueryFactory jpaQueryFactory;
 
     @BeforeEach
-    public void before() {
+    void before() {
         jpaQueryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
@@ -57,18 +60,127 @@ public class QuerydslBasicTest {
 
     @Test
     void startQuerydsl() {
-        QMember m = new QMember("m");
+        //QMember m = new QMember("m");
+        //QMember m = QMember.member;
 
         Member findMember = jpaQueryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) // 파라미터 바인딩 처리
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
 
+    @Test
+    void searchAndParam() {
+        Member findMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                        //.and(member.age.eq(10)))
+                        //.and(member.age.eq(10))
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+    @Test
+    void resultFetch() {
+        /*
+        List<Member> fetch = jpaQueryFactory
+                .selectFrom(member)
+                .fetch();
+        */
+
+        /*
+        Member fetchOne = jpaQueryFactory
+                .selectFrom(member)
+                .fetchOne();
+        */
+
+        /*
+        Member fetchFirst = jpaQueryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        */
+
+        QueryResults<Member> result = jpaQueryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        result.getTotal();
+        List<Member> content = result.getResults();
+
+
+        long total = jpaQueryFactory.
+                selectFrom(member)
+                .fetchCount();
+
+    }
+
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순 (DESC)
+     * 2. 회원 이름 올림차순 (ASC)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력 (Nulls Last)
+     */
+    @Test
+    void sort() {
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+
+    @Test
+    void paging() {
+        List<Member> result = jpaQueryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+
+    @Test
+    void paging2() {
+        QueryResults<Member> queryResults = jpaQueryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 
 
 }
+
